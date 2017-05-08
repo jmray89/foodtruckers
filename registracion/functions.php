34b53fc252f394ccdfbe92fd45cea115ee1b1aa2
Unknown <morel_f@yahoo.com.ar>
2017-05-08 00:32:53 -0300

    Registracion
    Muchachos le metí bastante a la registracion, echenle un ojo, diganme que opinan y si hay cosas que cambiar( faltan un par de cosas igual), estuve validando la registracion, ya se guarda en JSON y queda guardada la foto de perfil.

--- registracion/functions.php
+++ registracion/functions.php
@@ -3,37 +3,168 @@ session_start();
 
  function validarInformacion($datosUsuario){
 
-   $errores = [];
+  $errores = [];
+
+  $nombre = trim($datosUsuario["nombre"]);
 
    if (strlen($_POST["nombre"]) == 0) {
      $errores["nombre"] = "No llenaste tu nombre.";
-   }elseif (strlen($_POST["nombre"]) == 1){
+   }
+   elseif (strlen($_POST["nombre"]) == 1){
      $errores["nombre"] = "Tu nombre no puede estar compuesto por solo una letra.";
-   }   
+   }
+
+  $apellido = trim($datosUsuario["apellido"]);
+
    if (strlen($_POST["apellido"]) == 0) {
      $errores["apellido"] = "No llenaste tu apellido.";
-   }elseif (strlen($_POST["apellido"]) == 1){
+   }
+   elseif (strlen($_POST["apellido"]) == 1){
      $errores["apellido"] = "Tu apellido no puede estar compuesto por solo una letra.";
    }
+
+  $email = trim($datosUsuario["mail"]);
+
    if (strlen($_POST["mail"]) == 0) {
      $errores["mail"] = "No llenaste tu mail.";
-   }elseif (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
+   }
+   elseif (!filter_var($_POST["mail"], FILTER_VALIDATE_EMAIL)) {
      $errores["mail"] = "El mail que llenaste no es válido.";
    }
-   if (strlen($_POST["username"]) < 8) {
+
+  $usuario = trim($datosUsuario["username"]);
+
+   if (strlen($_POST["username"]) === 0) {
+     $errores["username"] = "No llenaste tu usuario.";
+   }
+   elseif (strlen($_POST["username"]) < 8) {
      $errores["username"] = "Tu nombre de usuario debe tener como mínimo 8 caracteres.";
    }
-   if (strlen($_POST["password"]) < 8) {
+
+  //  Contraseña //
+
+   if (strlen($_POST["password"]) === 0) {
+     $errores["password"] = "No llenaste tu contraseña.";
+   }
+   elseif (strlen($_POST["password"]) < 8) {
      $errores["password"] = "Tu contraseña debe tener como mínimo 8 caracteres.";
    }
+
+  //  Confirmar contraseña //
+
+  if (strlen($_POST["passRepeat"]) === 0) {
+    $errores["passRepeat"] = "No llenaste tu contraseña.";
+  }
+  elseif (strlen($_POST["passRepeat"]) < 8) {
+    $errores["passRepeat"] = "Tu contraseña debe tener como mínimo 8 caracteres.";
+  }
+
+  // Coincidir contraseñas //
+
    if ($_POST["password"] != $_POST["passRepeat"]) {
      $errores["passRepeat"] = "Tus contraseñas no coinciden.";
    }
 
+   if ($_POST["password"] != $_POST["passRepeat"] && $_POST["passRepeat"] != $_POST["password"]){
+     $errores["password"] = "Las dos contraseñas deben ser iguales.";
+   }
+
+   $img = $_FILES["imgPerfil"];
+
+       if ($img["name"] == "") {
+         $errores["img"] = "No elegiste un archivo.";
+       }
+
    return $errores;
  }
 
+// Guardar una foto //
 
+ function guardarImagen($unaImagen, $errores) {
 
+   if ($_FILES[$unaImagen]["error"] == UPLOAD_ERR_OK)
+   {
+
+     $nombre = $_FILES[$unaImagen]["name"];
+     $archivo = $_FILES[$unaImagen]["tmp_name"];
+
+     $ext = pathinfo($nombre, PATHINFO_EXTENSION);
+
+     if ($ext == "jpg" || $ext == "jpeg" || $ext == "png") {
+       $miArchivo = dirname(__FILE__);
+       $miArchivo = $miArchivo . "/subidos/";
+       $miArchivo = $miArchivo . $_POST["username"] . "." . $ext;
+       move_uploaded_file($archivo, $miArchivo);
+     }
+     else {
+       $errores["imgPerfil"] = "Este archivo no es una foto.";
+     }
+   } else {
+     //Acá hay error
+     $errores["imgPerfil"] = "No se pudo subir la foto.";
+   }
+   return $errores;
+ }
+
+ // Crear un usuario //
+
+ function crearUsuario($datosUsuario) {
+   $usuario = [
+     "nombre"   => $datosUsuario["nombre"],
+     "apellido" => $datosUsuario["apellido"],
+     "mail"     => $datosUsuario["mail"],
+     "username" => $datosUsuario["username"],
+     "password" => password_hash($datosUsuario["password"], PASSWORD_DEFAULT)
+   ];
+
+   $usuario["id"] = traerNuevoId();
+
+   return $usuario;
+ }
+
+ // Guardar un usuario //
+
+ function guardarUsuario($datosUsuario) {
+   $json = json_encode($datosUsuario);
+
+   $json = $json . PHP_EOL;
+
+   file_put_contents("usuarios.json", $json, FILE_APPEND);
+ }
+
+ // Traigo todos (con el usuario final saco su id) //
+
+ function traerTodos() {
+   // Leo el archivo
+   $archivo = file_get_contents("usuarios.json");
+
+  //  Lo divido por enters
+  $usuariosJSON = explode(PHP_EOL, $archivo);
+  // Quito el enter del final
+  array_pop($usuariosJSON);
+
+  $usuariosFinal = [];
+
+  // A cada linea la convierto de Json a Array
+  foreach ($usuariosJSON as $json) {
+    $usuariosFinal[] = json_decode($json, true);
+  }
+
+  return $usuariosFinal;
+}
+  // Traigo un nuevo Id (este se agrega al agregar un nuevo usuario)
+
+  function traerNuevoId() {
+    $usuarios = traerTodos();
+
+    if (count($usuarios) == 0) {
+      return 1;
+    }
+    $elUltimo = array_pop($usuarios);
+
+    $id = $elUltimo["id"];
+
+    return $id + 1;
+  }
 
  ?>
--- registracion/registracion.php
+++ registracion/registracion.php
@@ -1,21 +1,24 @@
 <?php
 require_once('functions.php');
 
-$nombre ="";
-$apellido ="";
-$mail ="";
-$username ="";
-$errores = [];
+$nombre   = "";
+$apellido = "";
+$mail     = "";
+$username = "";
+$errores  = [];
 
 if ($_POST) {
 
   $errores = validarInformacion($_POST);
 
-
   if (count($errores) == 0) {
-    header("Location:exito.php");exit;
+    $errores = guardarImagen("imgPerfil", $errores);
+  if (count($errores) == 0) {
+      $usuario = crearUsuario($_POST);
+      guardarUsuario($usuario);
+      header("Location:exito.php");exit;
+    }
   }
-
     if (!isset($errores["nombre"])) {
       $nombre = $_POST["nombre"];
     }
@@ -33,60 +36,171 @@ if ($_POST) {
 
  <!DOCTYPE html>
  <html>
-   <head>
-     <meta charset="utf-8">
-     <title>Form de registro</title>
-     <link rel="stylesheet" href="styles.css">
-   </head>
-   <body>
+ <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+ <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <title>Asociate</title>
+
+  <head>
+  <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+  <link rel="stylesheet" href="../styles/style.css" data-minify="1" />
+
+  <script src="../js/query.js"></script>
+  <script src="../js/utils.js"></script>
+
+  </head>
+  <body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse" aria-expanded="false"> <span class="sr-only">Toggle navigation</span>
+<span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+<a class="navbar-brand" href="index.php"> <img src="../images/logo-foodtruckers.png" alt="Foodtruckers" class="logo" /></a>
+  </div>
+  <div id="bs-navbar-collapse" class="collapse navbar-collapse">
+  <ul class="nav navbar-nav navbar-right">
+  <li><a href="index.php">Inicio</a></li>
+  <li><a href="index.php">Nosotros</a></li>
+  <li><a href="index.php">Eventos</a></li>
+  <li><a href="index.php">Foodtruckers</a></li>
+  <li><a href="index.php">Proovedores</a></li>
+  <li><a href="contacto.php">Contacto</a></li>
+  <li><a href="login/login.php">&vert; Login &vert;</a></li>
+  <li class="hidden-sm hidden-xs btn button_primary button_contact menu-item menu-item-type-post_type menu-item-object-page menu-item-125"><a href="registracion/registracion.php">Asociate</a></li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+
+
+  <header class="header-small" data-parallax="scroll" data-image-src="../images/foodtruckers-contacto.jpg">
+      <div id="slogan">
+          <p class="soustitre">Foodtruckers</p>
+        <h1>Unete a nosotros</h1></div>
+      <div id="gradient2">&nbsp;</div>
+  </header>
      <div class="main-container">
 
-       <div class="log-errors">
+       <!-- <div class="log-errors">
          <ul>
            <?php foreach($errores as $error): ?>
              <li><?=$error?></li>
            <?php endforeach; ?>
          </ul>
+       </div> -->
+       <br><br>
+       <form class="form-reg" action="registracion.php" method="post" enctype="multipart/form-data">
+         <section>
+           <article class="art.reg">
+              <label for="nombre">Nombre</label><br>
+              <input type="text" name="nombre" value="<?=$nombre?>">
+              <?php if (isset($errores["nombre"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["nombre"];?></span>
+              <?php } ?><br><br>
+           </article>
+              <label for="apellido">Apellido</label><br>
+              <input type="text" name="apellido" value="<?=$apellido?>">
+              <?php if (isset($errores["apellido"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["apellido"];?></span>
+              <?php } ?><br><br>
 
-       </div>
-       <form class="form-reg" action="registracion.php" method="post">
+          <!-- PONER UNO CON FECHA DE NACIMIENTO -->
 
-         <label for="nombre">Nombre</label><br>
-         <input type="text" name="nombre" value="<?=$nombre?>"><?php if (isset($errores["nombre"])) {
-           echo $errores["nombre"];
-         } ?><br><br>
-
-         <label for="apellido">Apellido</label><br>
-         <input type="text" name="apellido" value="<?=$apellido?>"><?php if (isset($errores["apellido"])) {
-           echo $errores["apellido"];
-         } ?><br><br>
-
-         <label for="mail">E-mail</label><br>
-         <input type="mail" name="mail" value="<?=$mail?>"><?php if (isset($errores["mail"])) {
-           echo $errores["mail"];
-         } ?><br><br>
-
-         <label for="username">Nombre de Usuario</label><br>
-         <input type="text" name="username" value="<?=$username?>"><?php if (isset($errores["username"])) {
-           echo $errores["username"];
-         } ?><br><br>
-
-         <label for="password">Contraseña</label><br>
-         <input type="password" name="password" value=""><?php if (isset($errores["password"])) {
-           echo $errores["password"];
-         } ?><br><br>
-
-         <label for="pass-repeat">Confirmar contraseña</label><br>
-         <input type="password" name="passRepeat" value=""><?php if (isset($errores["passRepeat"])) {
-           echo $errores["passRepeat"];
-         } ?><br><br>
-
-         <input type="submit" name="submit" value="Enviar">
-
-
+           <article class="art.reg">
+              <label for="mail">E-mail</label><br>
+              <input type="mail" name="mail" value="<?=$mail?>">
+              <?php if (isset($errores["mail"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["mail"];?></span>
+              <?php } ?><br><br>
+           </article>
+           <article class="art.reg">
+              <label for="username">Nombre de Usuario</label><br>
+              <input type="text" name="username" value="<?=$username?>">
+              <?php if (isset($errores["username"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["username"];?></span>
+              <?php } ?><br><br>
+           </article>
+           <article class="art.reg">
+              <label for="password">Contraseña</label><br>
+              <input type="password" name="password" value="">
+              <?php if (isset($errores["password"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["password"];?></span>
+              <?php } ?><br><br>
+           </article>
+           <article class="art.reg">
+              <label for="pass-repeat">Confirmar contraseña</label><br>
+              <input type="password" name="passRepeat" value="">
+              <?php if (isset($errores["passRepeat"])) { ?>
+              <br>
+              <span style="color:red"><?=$errores["passRepeat"];?></span>
+              <?php } ?><br><br>
+           </article>
+           <article class="art.reg">
+              <label for="">Imagen de perfil</label><br>
+              <input type="file" name="imgPerfil" value=""><br><br>
+           </article>
+           <article class="art.reg">
+              <input type="submit" name="submit" value="Enviar">
+           </article>
+         </section>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
        </form>
 
      </div>
+     <footer class="negativeMargin">
+         <div class="container">
+             <div class="row">
+                 <div class="col-md-6"> <img src="../images/logo-foodtruckers-footer.png" alt="Foodtruckers" class="logo" />
+                     <div class="textwidget">
+                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at finibus dui, ut luctus mi. Aenean bibendum odio semper turpis malesuada, sed laoreet urna rhoncus.</p>
+                     </div>
+                     <div class="row" id="sitemap">
+                         <div class="col-md-4">
+     <h4>Nosotros</h4>
+     <div class="menu-activites-container">
+     <ul class="list-unstyled">
+     <li class="menu-item"><a href="#">Empresa</a></li>
+     <li><a href="preguntas-frecuentes.php">Preguntas frecuentes</a></li>
+     <li><a href="contacto.php">Contacto</a></li>
+                                 </ul>
+                             </div>
+                         </div>
+                         <div class="col-md-4">
+     <h4>Legal</h4>
+     <div class="menu-emersion-container">
+     <ul class="list-unstyled">
+     <li><a href="#">Informacion legal</a></li>
+     <li><a href="#">Términos y condiciones </a></li>
+
+
+                                 </ul>
+                             </div>
+                         </div>
+       <div class="col-md-4">
+       <h4>Redes Sociales</h4>
+       <div class="menu-contact-container">
+       <ul id="menu-contact" class="list-unstyled">
+       <li><a href="https://www.facebook.com/Foodtruckers/">Facebook</a></li>
+       <li><a href="https://www.twitter.com/Foodtruckers/">Twitter</a></li>
+       <li><a href="https://www.instagram.com/Foodtruckers">Instagram</a></li>
+                                 </ul>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+               </div>
+         </div>
+     </footer>
 
    </body>
  </html>
